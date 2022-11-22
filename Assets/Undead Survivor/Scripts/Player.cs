4734382ed80e704634492d474ef15387e23b073a
wlsdpcks <80802907+wlsdpcks@users.noreply.github.com>
2022-11-23 00:30:39 +0900

    03.애니메이션 제작하기

--- Assets/Undead Survivor/Scripts/Player.cs
+++ Assets/Undead Survivor/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
@@ -8,23 +9,34 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-    }
-
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate() // 물리 연산 프레임마다 호출되는 생명주기 함수
     {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         // fixedDeltaTime:물리 프레임 하나가 소비한 시간
         rigid.MovePosition(rigid.position + nextVec); // MovePosition은 위치이동이라 현재 위치도 더해줘야함.
 
+    }
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        anim.SetFloat("Speed", inputVec.magnitude); // magnitude : 벡터의 순수한 크기 값
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
